9c3be5848893faf346d7def6a60c5edfba96bb18
Jon Nordby <jononor@gmail.com>
2014-08-24 23:19:49 +0200

    FBP protocol: Implement listing of components
    NoFlo UI can now show the components included with javafbp

--- src/main/java/com/jpmorrsn/fbp/engine/Runtime.java
+++ src/main/java/com/jpmorrsn/fbp/engine/Runtime.java
@@ -2,7 +2,16 @@ package com.jpmorrsn.fbp.engine;
 
 import com.jpmorrsn.fbp.components.WriteToConsole;
 import com.jpmorrsn.fbp.engine.Network;
+import com.jpmorrsn.fbp.engine.Component;
+import com.jpmorrsn.fbp.engine.OutputPort;
+import com.jpmorrsn.fbp.engine.InputPort;
 import com.jpmorrsn.fbp.examples.components.GenerateTestData;
+
+import java.io.FileReader;
+import java.io.Reader;
+import java.lang.Exception;
+import java.lang.Iterable;
+import java.lang.Override;
 import java.util.Map;
 import java.util.List;
 import java.util.HashMap;
@@ -14,6 +23,125 @@ import org.java_websocket.server.*;
 import org.json.*;
 
 final public class Runtime {
+
+    public static class Util {
+
+        public static class JSONObjectKeysIterable implements Iterable {
+            JSONObject mObject;
+            JSONObjectKeysIterable(JSONObject o) {
+                mObject = o;
+            }
+
+            public java.util.Iterator<String> iterator() {
+                return mObject.keys();
+            }
+        }
+
+    }
+
+    public static class ComponentLibrary {
+
+        private HashMap<String, Class> mComponents;
+
+        ComponentLibrary() {
+            mComponents = new HashMap<String, Class>();
+
+            JSONObject components = null;
+            try {
+                Reader in = new FileReader("fbp.json"); // XXX: relative to cwd
+                JSONTokener tokener = new JSONTokener(in);
+                JSONObject root = new JSONObject(tokener);
+                components = root.optJSONObject("components");
+            } catch (Exception e) {
+                System.err.println("Unable to parse fbp.jon: " + e.toString());
+            }
+
+            if (components != null) {
+                String baseLib = "com.jpmorrsn.fbp.components"; // FIXME: define in fbp.json
+                Iterable<String> keys = new Runtime.Util.JSONObjectKeysIterable(components);
+                for (String name : keys) {
+                    final String className = baseLib + "." + name;
+                    try {
+                        Class c = Class.forName(className);
+                        mComponents.put(name, c);
+                    } catch (Exception e) {
+                        System.err.println("Cannot load component " + name + ": " + e.toString());
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        public Map<String, Class> getComponents() { return mComponents; }
+
+        private static List<InPort> getInports(Class comp) {
+            ArrayList<InPort> ret = new ArrayList<InPort>();
+            InPort p = (InPort)comp.getAnnotation(InPort.class);
+            if (p != null) {
+                ret.add(p);
+            }
+            InPorts ports = (InPorts)comp.getAnnotation(InPorts.class);
+            if (ports != null) {
+                for (InPort ip : ports.value()) {
+                    ret.add(ip);
+                }
+            }
+            return ret;
+        }
+        private static List<OutPort> getOutports(Class comp) {
+            ArrayList<OutPort> ret = new ArrayList<OutPort>();
+            OutPort p = (OutPort)comp.getAnnotation(OutPort.class);
+            if (p != null) {
+                ret.add(p);
+            }
+            OutPorts ports = (OutPorts)comp.getAnnotation(OutPorts.class);
+            if (ports != null) {
+                for (OutPort op : ports.value()) {
+                    ret.add(op);
+                }
+            }
+            return ret;
+        }
+
+        public JSONObject getComponentInfoJson(String componentName) throws JSONException {
+            // Have to instantiate the component to introspect :(
+            Class componentClass = mComponents.get(componentName);
+
+            // Top-level
+            JSONObject def = new JSONObject();
+            def.put("name", componentName);
+            // def.putString("description", ""); TODO
+            def.put("subgraph", false); // TODO: support subgraphs
+            // def.putBoolean("icon", ""); // TODO: allow components to specify icon
+
+            // InPorts
+            JSONArray inPorts = new JSONArray();
+            for (InPort port : ComponentLibrary.getInports(componentClass)) {
+                JSONObject portInfo = new JSONObject();
+                portInfo.put("id", port.value());
+                portInfo.put("type", "any"); // TODO: annotate more specifically
+                portInfo.put("description", port.description());
+                portInfo.put("addressable", port.arrayPort());
+                portInfo.put("required", !port.optional());
+                inPorts.put(portInfo);
+            }
+            def.put("inPorts", inPorts);
+
+            // OutPorts
+            JSONArray outPorts = new JSONArray();
+            for (OutPort port : ComponentLibrary.getOutports(componentClass)) {
+                JSONObject portInfo = new JSONObject();
+                portInfo.put("id", port.value());
+                portInfo.put("type", "any"); // TODO: annotate more specifically
+                portInfo.put("description", port.description());
+                portInfo.put("addressable", port.arrayPort());
+                portInfo.put("required", !port.optional());
+                outPorts.put(portInfo);
+            }
+            def.put("outPorts", outPorts);
+
+            return def;
+        }
+    }
 
     private static class Definition {
 
@@ -99,8 +227,13 @@ final public class Runtime {
                 sendFbpResponse("runtime", "runtime", p, socket);
             } else if (protocol.equals("component") && command.equals("list")) {
 
+                Runtime.ComponentLibrary lib = new Runtime.ComponentLibrary(); // TEMP: move out, object lifetime should be that of process
+                for (String name : lib.getComponents().keySet()) {
+                    JSONObject def = lib.getComponentInfoJson(name);
+                    sendFbpResponse("component", "component", def, socket);
+                }
             } else {
-
+                System.err.println("Unknown FBP protocol message: " + protocol + ":" + command);
             }
         }
 
@@ -161,8 +294,5 @@ final public class Runtime {
         s.start();
         System.out.println("Listening on port: " + s.getPort());
     }
-    // Class.forName(className);
 
-    // nodes: id, .class
-    // addNode, delNode,
 }
