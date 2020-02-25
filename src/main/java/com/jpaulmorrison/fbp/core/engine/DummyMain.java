24dc4ff519aa13caf21d9af1e0f198b30bc82431
Paul Morrison <jpaulmorr@gmail.com>
2020-02-24 20:29:16 -0500

    Added DummyMain.java to keep Gradle happy (needed?)

---
+++ src/main/java/com/jpaulmorrison/fbp/core/engine/DummyMain.java
@@ -0,0 +1,10 @@
+package com.jpaulmorrison.fbp.core.engine;
+
+public class DummyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Dummy main - should not be invoked"); 
+	}
+
+}
